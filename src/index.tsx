62c190816c187f3ea586a6d6498eb8e984610d14
Felipe Lourenco <felipe.v.lourenco@gmail.com>
2022-03-04 14:27:28 +0100

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import '@formatjs/intl-relativetimeformat/polyfill'
+import '@formatjs/intl-relativetimeformat/locale-data/en' // locale-data for en
 
 ReactDOM.render(
   <React.StrictMode>
