12380e49c0df8d93f64c1e91a7afe01f9c85dc49
Felipe Lourenco <felipe.v.lourenco@gmail.com>
2022-03-04 15:09:18 +0100

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-// import '@formatjs/intl-relativetimeformat/polyfill'
-// import '@formatjs/intl-relativetimeformat/locale-data/en' // locale-data for en
+import '@formatjs/intl-getcanonicallocales/polyfill'
+import '@formatjs/intl-locale/polyfill'
+import '@formatjs/intl-pluralrules/polyfill'
+import '@formatjs/intl-pluralrules/locale-data/en' // locale-data for en
+import '@formatjs/intl-relativetimeformat/polyfill'
+import '@formatjs/intl-relativetimeformat/locale-data/en' // locale-data for en
 
 ReactDOM.render(
   <React.StrictMode>
