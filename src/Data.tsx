3ec7c4038ef87ae0e3ab0279ddc4b5f18041c3d7
Felipe Lourenco <felipe.lourenco@ingenico.com>
2022-03-15 14:12:54 +0100

    test

--- src/Data.tsx
+++ src/Data.tsx
@@ -5,6 +5,14 @@ import { getNumberOfWeeksFromToday } from './util'
 export const Data = () => {
   const { formatRelativeTime } = useIntl()
   const data = '2019-04-12T11:42:51+0200'
+
+
+  console.log('===================');
+  console.log(data);
+  console.log(getNumberOfWeeksFromToday(data));
+  console.log(formatRelativeTime(getNumberOfWeeksFromToday(data)));
+  console.log('===================');
+
   // const weeks = -153
 
   return (
