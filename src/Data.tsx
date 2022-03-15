7a3feba8c4c3daa54e64e50b5ec091697a1413b4
Felipe Lourenco <felipe.lourenco@ingenico.com>
2022-03-15 15:42:49 +0100

    test

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useIntl } from 'react-intl'
-import { getNumberOfWeeksFromToday } from './util'
-import _format from 'date-fns/format';
+import { getNumberOfWeeksFromToday, isSafari, formatDateSafari } from './util'
 import _parseISO from 'date-fns/parseISO';
 
 export const Data = () => {
   const { formatRelativeTime } = useIntl()
   const data = '2019-04-12T11:42:51+0200'
-
+  const parse = _parseISO(data).toUTCString()
 
   console.log('===================');
   console.log(data);
   console.log(getNumberOfWeeksFromToday(data));
-  console.log(formatRelativeTime(getNumberOfWeeksFromToday(data)));
+  console.log(formatRelativeTime(getNumberOfWeeksFromToday(data), 'week', {
+    style: 'long',
+  }));
   console.log('===================');
-  console.log(_format(_parseISO(data), "yyyy-MM-dd'T'HH:mm:ss.SSSxxx"));
+  console.log(parse);
+  console.log(getNumberOfWeeksFromToday(parse));
+  console.log(formatRelativeTime(getNumberOfWeeksFromToday(parse), 'week', {
+    style: 'long',
+  }));
   console.log('===================');
 
-  // const weeks = -153
+  const formattedDate = useMemo(() => {
+    return formatRelativeTime(getNumberOfWeeksFromToday(
+      isSafari() ? formatDateSafari(data) : data
+    ), 'week', {
+      style: 'long',
+    })
+  }, [formatRelativeTime]);
 
   return (
-    <h1>{formatRelativeTime(getNumberOfWeeksFromToday(data), 'week', {
-      style: 'long',
-    })}</h1>
+    <h1>{formattedDate}</h1>
   )
 }
--- src/util.ts
+++ src/util.ts
@@ -8,3 +8,25 @@ export const getNumberOfWeeksBetween = (date1: Date, date2: Date) =>
 
 export const getNumberOfWeeksFromToday = (date: string) =>
   getNumberOfWeeksBetween(new Date(date), new Date())
+
+export const isSafari = () => {
+  const userAgent = navigator.userAgent.toLowerCase()
+  return (
+    /safari/.test(userAgent) &&
+    /applewebkit/.test(userAgent) &&
+    /version\/13./.test(userAgent) &&
+    !/chrome/.test(userAgent)
+  )
+}
+
+export const formatDateSafari = (date: string) => {
+  const values = date.split(/[^0-9]/)
+  return new Date(
+    parseInt(values[0], 10),
+    parseInt(values[1], 10) - 1,
+    parseInt(values[2], 10),
+    parseInt(values[3], 10),
+    parseInt(values[4], 10),
+    parseInt(values[5], 10)
+  ).toUTCString()
+}
